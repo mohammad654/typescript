6e11ee6a4e94ef64cddd9fe38f1fc97f7d145e5d
Mohammad Ali <Mohammad.ali.shikhi.55@gmail.com>
2023-01-11 00:59:03 +0100

    first commit

---
+++ Practice/src/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// const task = document.getElementById("task");
+// function fun1() {
+//     console.log("value is ");
+//   }
+// function fun() {
+//   // console.log("Task is : ", task.value);
+// }
+// ----------------------------------------------------
---
+++ Practice/src/index.ts
@@ -0,0 +1,11 @@
+
+
+// const task = document.getElementById("task");
+
+// function fun1() {
+//     console.log("value is ");
+//   }
+// function fun() {
+//   // console.log("Task is : ", task.value);
+// }
+// ----------------------------------------------------
